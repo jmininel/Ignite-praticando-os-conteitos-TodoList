68678247ee8975de08eb822bfa8ea86874e5c207
jmininel <ju.mininel@hotmail.com>
2022-10-26 22:42:07 -0300

    update Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,34 @@
 import  Logo  from '../../assets/Logo.svg';
 import { AiOutlinePlusCircle } from 'react-icons/ai'
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-export function Header() {
+interface Props {
+    onAddTask: (taskTitle: string) => void;
+}
+
+export function Header({onAddTask}: Props) {
+    const [title, setTitle] = useState("");  //estado do titulo atual
+     
+    function handleSubmit(event: FormEvent) {
+       event.preventDefault();
+
+       onAddTask(title); //adiona o novo imput
+       setTitle("");  //limpar o input
+    }
+
+    function OnChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+        setTitle(event.target.value)
+    }
+
     return(
         <header className=" flex items-center justify-center relative bg-gray-700 w-full h-[12.5rem]">
             <img src={Logo} className="mb-[3.375rem]" />
 
-            <form action="" className="flex w-full  absolute  max-w-[46rem] gap-2  bottom-[calc(-3.375rem/2)] ">
+            <form onSubmit={handleSubmit} className="flex w-full  absolute  max-w-[46rem] gap-2  bottom-[calc(-3.375rem/2)] ">
                 <input
-                  className="h-full flex-1 border-none rounded-lg py-4 bg-gray-500 mr-2 placeholder:text-gray-300 placeholder:px-4"
+                 onChange={OnChangeTitle}
+                 value={title}
+                 className="h-full flex-1 border-none rounded-lg py-4 bg-gray-500 mr-2 placeholder:text-gray-300 placeholder:px-4"
                  placeholder="Adicionar uma tarefa"
                 />
                 <button className="flex justify-center items-center border-none h-full px-4 py-4 gap-[0.375rem] font-bold text-xs bg-blue-200 rounded-lg">
